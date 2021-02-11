9e43d17dcafca33810d1ec6741ddb846a86f5eab
axylos <robertdraketalley@gmail.com>
2021-02-10 22:24:07 -0800

    init rails

---
+++ db/migrate/20210211061919_create_user.rb
@@ -0,0 +1,10 @@
+class CreateUser < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users, id: :uuid do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :gh_username
+    end
+  end
+end
